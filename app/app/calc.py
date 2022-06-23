7288da5843c30b14b1780a11a3aaa35868abdb47
Harsha <harsha@192.168.0.113>
2022-06-24 01:25:28 +0530

    Database Docker setup

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculatot functions
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def substract(x, y):
+    """Subtract x from y and return resutl."""
+    return y - x
